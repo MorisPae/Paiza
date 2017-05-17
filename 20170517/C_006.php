ed713cd10028450f4d32ab40364f339f028acf0c
MorisPae <kiyofumi.mori138@gmail.com>
2017-05-17 10:51:25 +0900

    margeの練習

--- 20170517/C_006.php
+++ 20170517/C_006.php
@@ -2,6 +2,7 @@
     //ハイスコアランキング
     $users = trim(fgets(STDIN)); //Nアイテムの個数 Mユーザーの人数 Kトップ
     $points = trim(fgets(STDIN)); //N個のパラメータの各係数Ci
-    $
+    for ($i = 0;)
+    $counts = trim(fgets(STDIN)); //3行目からM+2行目までの各ユーザのXi数値
 
  ?>
